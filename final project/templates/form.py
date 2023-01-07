588a1d158a281bc9a4e745f45fda2c427ee858af
Ping-An Lin <luckyanthonyan@gmail.com>
2023-01-07 19:25:55 +0800

    remove useless files

--- final project/templates/form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-
-
-class MyForm(FlaskForm):
-    Sexual = SelectField(u'性別', choices=[('男','Male'),('女','Female'),('不願透漏','No Answer')])
-    Age = SelectField(u'年齡', choices=['10~15','15~20','20~30'])
-    submit = SubmitField('Submit')
